892bb36c5bf19132888fca74725d24426770a2a6
Skyler <skyler.d.y.s@gmail.com>
2025-05-05 11:32:35 +0800

    completed hw0a

--- hw0a/src/JavaExercises.java
+++ hw0a/src/JavaExercises.java
@@ -12,7 +12,14 @@ public class JavaExercises {
      * The first row contains 1 star, the second 2 stars, and so on.
      */
     public static void starTriangle() {
-        // TODO: Fill in this function
+        int n = 5;
+        StringBuilder line = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            line.append(" ".repeat(n - i));
+            line.append("*".repeat(i));
+            line.append("\n");
+        }
+        System.out.println(line);
     }
 
     /**
@@ -20,7 +27,13 @@ public class JavaExercises {
      * Example: printIndexed("hello") -> h4e3l2l1o0
      */
     public static void printIndexed(String s) {
-        // TODO: Fill in this function
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i));
+            System.out.print(s.length() - i - 1);
+            // Or System.out.print(s.charAt(i) + "" + (s.length() - i - 1));
+            // Or System.out.printf("%c%d", s.charAt(i), (s.length() - i - 1));
+        }
+        System.out.println();
     }
 
     /**
@@ -28,32 +41,46 @@ public class JavaExercises {
      * Example: stutter("hello") -> "hheelllloo"
      */
     public static String stutter(String s) {
-        // TODO: Fill in this function
-        return null;
+        if (s == null) {
+            return null;
+        }
+        StringBuilder ss = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            ss.append(s.charAt(i)).append(s.charAt(i));
+        }
+        return ss.toString();
     }
 
     /**
      * Determines the quadrant of a Cartesian coordinate (x, y).
      * Returns:
-     *   1 for the first quadrant (x > 0, y > 0),
-     *   2 for the second quadrant (x < 0, y > 0),
-     *   3 for the third quadrant (x < 0, y < 0),
-     *   4 for the fourth quadrant (x > 0, y < 0),
-     *   0 if the point lies on an axis.
+     * 1 for the first quadrant (x > 0, y > 0),
+     * 2 for the second quadrant (x < 0, y > 0),
+     * 3 for the third quadrant (x < 0, y < 0),
+     * 4 for the fourth quadrant (x > 0, y < 0),
+     * 0 if the point lies on an axis.
      */
     public static int quadrant(int x, int y) {
-        // TODO: Fill in this function
+        if (x > 0 && y > 0) {
+            return 1;
+        } else if (x < 0 && y > 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        } else if (x > 0 && y < 0) {
+            return 4;
+        }
         return 0;
     }
 
-    public static void main(String[] args) {
-        starTriangle();
-        printIndexed("hello");
-        System.out.println(stutter("hello"));
-        System.out.println(quadrant(3, 4));  // Output: 1
-        System.out.println(quadrant(-3, 4)); // Output: 2
-        System.out.println(quadrant(-3, -4));// Output: 3
-        System.out.println(quadrant(3, -4)); // Output: 4
-        System.out.println(quadrant(0, 5));  // Output: 0
+        public static void main (String[]args){
+            starTriangle();
+            printIndexed("hello");
+            System.out.println(stutter("hello"));
+            System.out.println(quadrant(3, 4));  // Output: 1
+            System.out.println(quadrant(-3, 4)); // Output: 2
+            System.out.println(quadrant(-3, -4));// Output: 3
+            System.out.println(quadrant(3, -4)); // Output: 4
+            System.out.println(quadrant(0, 5));  // Output: 0
+        }
     }
-}
